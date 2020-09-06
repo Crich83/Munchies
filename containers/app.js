1fc761af63e6eac46253df6bb0a12bf422303a06
Crich83 <57454470+Crich83@users.noreply.github.com>
2020-09-06 06:23:16 -0400

    Create app.js

---
+++ containers/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import MenuList from '../components/menu-list'
+import Cart from '../components/cart'
+  
+const app = () => (
+<div>
+    <h1>Munchies</h1>
+    <hr/>
+    <div className="main-left">
+        <h2>Menu</h2>
+        <MenuList/>
+    </div>
+
+    <div className="main-right">
+        <h2>Order Summary:</h2>
+        <Cart/>
+    </div>
+</div>
+)
+
+export default app;
