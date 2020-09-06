374a433df0a277e92758c1c469219b4eac5572b3
Crich83 <57454470+Crich83@users.noreply.github.com>
2020-09-06 06:29:42 -0400

    Create foodItems-data.js

---
+++ data/foodItems-data.js
@@ -0,0 +1,67 @@
+export default function(){
+    return[
+        {
+            "id": 1, "title": "Hamburger",
+            "description": "Lettuce,Tomatoe,Fried Onion", 
+            "price": 5.00, 
+            "Categories": ["Fast food", ""], 
+            "thumbnail": "https://magnoliadays.com/wp-content/uploads/2016/07/Pimento-Cheese-Burger-1-250px.jpg"
+        },
+        {
+            "id": 2, 
+            "title": "French Fries", 
+            "description": "French Fries - You can add Cheese!", 
+            "price": 2.00, 
+            "Categories": ["Fast food", ""], 
+            "thumbnail": "https://photo.foodgawker.com/wp-content/uploads/2012/06/942107.jpeg"
+        },
+        {
+            "id": 3, 
+            "title": "Veggie Pizza", 
+            "description": "Choose the veggies you want!", 
+            "price": 12.00, 
+            "Categories": ["Veggie Pizza"], 
+            "thumbnail": "https://www.hiddenvalley.com/wp-content/uploads/2016/02/Friday-Night-Cheesy-Veggie-Pizzas-250x250-c-default.jpg"
+        },
+        {
+            "id": 4, 
+            "title": "Chicken Pizza", 
+            "description": "Thin Crust with Veggies", 
+            "price": 12.00, 
+            "Categories": ["Pizza"], 
+            "thumbnail": "https://s3-media2.fl.yelpcdn.com/bphoto/Z8AhM4UkDoF8sLp9r0dnZg/ls.jpg"
+        },
+        {
+            "id": 5, 
+            "title": "Milk Shake", 
+            "description": "Salty Caramel Yogurt Shake", 
+            "price": 4.00, 
+            "Categories": ["Drink", "Milk shake"], 
+            "thumbnail": "https://www.howsweeteats.com/wp-content/uploads/2017/09/chobani-caramel-shake-9-250x250.jpg"
+        },
+        {
+            "id": 6, 
+            "title": "Chicken Pasta", 
+            "description": "Chicken cutlets,Mixed Veggies", 
+            "price": 9.00, 
+            "Categories": ["Chicken Pasta", "italian"], 
+            "thumbnail": "http://www.countrycleaver.com/wp-content/uploads/2017/07/Pesto-Chicken-Pasta-Skillet-with-Asparagus-and-Tomatoes-250x250.jpg"
+        },
+        {
+            "id": 7, 
+            "title": "Lasagna", 
+            "description": "Beef Lasagne with Rich Pesto Sauce", 
+            "price": 11.00, 
+            "Categories": ["Beef Lasagne", "italian"], 
+            "thumbnail": "http://makingthymeforhealth.com/wp-content/uploads/2017/12/Butternut-Squash-Kale-Lasagna-_-250x250.jpg"
+        },
+        {
+            "id": 8, 
+            "title": "Mojito", 
+            "description": "Salty Mojito with Lime", 
+            "price": 7.00, 
+            "Categories": ["Beverage", "Alcohol"], 
+            "thumbnail": "http://www.nephroplus.com/wp-content/uploads/2017/04/Virgin-Mojito-250x250.gif"
+        }
+    ]
+}
